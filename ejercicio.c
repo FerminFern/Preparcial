78f13ebd5002ce5f8e8901ad78f79cf725ef159e
Ferminfern <fermin.fernandez@alu.inspt.utn.edu.ar>
2024-05-29 21:48:54 -0300

    last commit

--- ejercicio.c
+++ ejercicio.c
@@ -1,4 +1,4 @@
-// Consigna:  el usuaio deberá ingresar dos valores y el programa devolvera el maximo y el mínimo,
+// Consigna: devolver el maximo y el mínimo de un conjunto de numeros ingresados por el usuario,
 // si el usuario ingresa un 0 el programa se finaliza, caso contrario se continua ejecutando
 
 #include <stdio.h>
@@ -14,46 +14,52 @@ int main(void)
 {
     int num, max, min, flag = 1;
 
-    do
+    cargarDato(&num);
+    while (num)
     {
-        cargarDato(&num);
-        if (num)
+        if (flag)
         {
-            if (flag)
-            {
-                max = num;
-                min = num;
-                flag = 0;
-            }
-            else
-            {
-                max = maximo(max, num);
-                min = minimo(min, num);
-            }
-
-            p("El valor max es: %d y el min es: %d",max, min);
-        } else {
-            p("Ha salido del sistema");
+            max = num;
+            min = num;
+            flag = 0;
         }
-    } while (num);
+        else
+        {
+            max = maximo(max, num);
+            min = minimo(min, num);
+        }
+        cargarDato(&num);
+    };
+
+    if (flag)
+    {
+        p("Ha salido del sistema");
+    }
+    else
+    {
+        p("El valor max es: %d y el min es: %d", max, min);
+    }
 
     return 0;
 };
 
-void cargarDato(int* num){
+void cargarDato(int *num)
+{
     p("\nIngrese un valor: ");
     s("%d", num);
 };
 
-int maximo(int max, int num){
-    if(max > num)
+int maximo(int max, int num)
+{
+    if (max > num)
         return max;
     else
         return num;
 };
 
-int minimo(int min, int num){
-    if(min < num)
+int minimo(int min, int num)
+{
+    if (min < num)
         return min;
     else
         return num;
